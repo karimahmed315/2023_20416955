7ff43c69786054194c16e3c3f45444fe57c2e0d5
Karim Abdou <efyka4@nottingham.ac.uk>
2024-02-13 11:34:07 +0000

    3 Part Executable

---
+++ Worksheet3/adder.h
@@ -0,0 +1,6 @@
+//The next two lines prevent including adder.h more than once in any .cpp file
+#ifndef MATHSLIB_ADDER_H
+	#define MATHSLIB_ADDER_H
+	//function prototype
+	int add (int a, int b );
+#endif
