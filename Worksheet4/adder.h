00e76318150e8794ba8f3b53189807ae9cd12496
Karim Abdou <efyka4@nottingham.ac.uk>
2024-03-05 10:42:35 +0000

    Added Wksh4 Source Files

---
+++ Worksheet4/adder.h
@@ -0,0 +1,16 @@
+#ifndef MATHSLIB_ADDER_H
+#define MATHSLIB_ADDER_H
+
+/** @file
+ * This file contains the declarations of all exported functions in the maths library.
+ */
+
+/** Adding function [Brief description]
+ * This function adds two numbers [More detail]
+ * @param a is the first number [Parameter definition]
+ * @param b is the second number [Parameter definition]
+ * @return sum of a and b [Return value description]
+ */
+int add(int a, int b);
+
+#endif
